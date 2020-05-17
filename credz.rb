0fcb3c73c919b58f234451e53e3c239c72f81798
Ryan Graham <rgraham@blvd.co>
2020-05-17 10:43:25 -0700

    bump release version and pass patch version override to cmake

--- credz.rb
+++ credz.rb
@@ -3,9 +3,10 @@
 class Credz < Formula
   desc 'AWS Okta Assume Role CLI'
   homepage 'https://github.com/ryangraham/credz'
-  url 'https://github.com/ryangraham/credz/archive/v0.0.1-cd4dd01.tar.gz'
-  sha256 '81a13c5406eb0d7441c511548826ce9717d809c194d93b10406f8fbda74fa19b'
+  url 'https://github.com/ryangraham/credz/archive/v0.0.1-2241c74.tar.gz'
+  sha256 'd8651106fd60701f451ccb9cb458a66d99ee3594e4c5da675ec0d73325db4bba'
   head 'https://github.com/ryangraham/credz.git', branch: 'master'
+  version '0.0.1-2241c74'
 
   depends_on 'cmake'
   depends_on 'boost'
@@ -13,13 +14,15 @@ class Credz < Formula
   depends_on 'nlohmann/json/nlohmann_json'
 
   def install
+    patch_version = version.to_s.split('-')[1]
     mkdir 'credz-build' do
-      system 'cmake', '..', *std_cmake_args
+      system 'cmake', '..', "-DPATCH_VERSION=#{patch_version}", *std_cmake_args
       system 'make', 'install'
     end
   end
 
   test do
     system "#{bin}/credz -v"
+    assert_match stable.version.to_s, shell_output("#{bin}/credz -v")
   end
 end
